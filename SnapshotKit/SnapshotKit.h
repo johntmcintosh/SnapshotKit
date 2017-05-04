a9498c5a64e57efe9eaf506b81598fc908a24376
John McIntosh <john@freetextbooks.com>
2017-05-04 14:38:30 -0500

    Create empty project

---
+++ SnapshotKit/SnapshotKit.h
@@ -0,0 +1,19 @@
+//
+//  SnapshotKit.h
+//  SnapshotKit
+//
+//  Created by John McIntosh on 5/4/17.
+//  Copyright © 2017 John T McIntosh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SnapshotKit.
+FOUNDATION_EXPORT double SnapshotKitVersionNumber;
+
+//! Project version string for SnapshotKit.
+FOUNDATION_EXPORT const unsigned char SnapshotKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SnapshotKit/PublicHeader.h>
+
+
