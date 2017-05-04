5aa71c13d604c71562d68b9c2b58d18773d2e879
John McIntosh <john@freetextbooks.com>
2017-05-04 15:06:04 -0500

    Install pods and setup for linking classes to test target

---
+++ Podfile
@@ -0,0 +1,12 @@
+platform :ios, '9.0'
+
+target 'SnapshotKit' do
+    pod 'FBSnapshotTestCase', '2.1.4'
+    pod 'Sourcery', '0.6.0'
+    target 'SnapshotKitTests' do
+        inherit! :search_paths
+    end
+end
+
+use_frameworks!
+inhibit_all_warnings!
---
+++ SnapshotKit/SnapshotKitTestController.swift
@@ -0,0 +1,14 @@
+//
+//  SnapshotKitTestController.swift
+//  SnapshotKit
+//
+//  Created by John McIntosh on 5/4/17.
+//  Copyright © 2017 John T McIntosh. All rights reserved.
+//
+
+import Foundation
+
+
+public class SnapshotKitTestController {
+    
+}
